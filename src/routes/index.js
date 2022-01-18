6e1a75bad50d76a06cda571e8346791b69065541
joelouisbm <74831269+joelouisbm@users.noreply.github.com>
2022-01-18 12:11:26 -0500

    Se agrega controlador de usuarios y se enruta desde el main hasta la nueva funciòn

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
+const user = require("./user");
+
 module.exports.initRoutes = (app) => {
   app.get("/", (req, res) => {
     res.json({
       name: "Api Test",
     });
   });
-  
+
+  app.use("/api/user", user);
+
   app.use("*", (req, res) => {
     res.json({
       code: 404,
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.json({
-    message: "hello from user router",
-  });
-});
+const controller = require("../controllers/user");
+router.get("/", controller.find);
 module.exports = router;
