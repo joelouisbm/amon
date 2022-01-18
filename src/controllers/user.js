cd270e7f78b26a664b91ec5c03996d13c4655f5c
joelouisbm <74831269+joelouisbm@users.noreply.github.com>
2022-01-18 12:43:40 -0500

    Cambio de saludo en el controlador de usuario y se agrega funcion nueva de crear usuarios

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,7 +1,13 @@
 module.exports.find = (req, res) => {
   try {
     res.status(200).json({
-      message: "Hello from User Controller",
+      message: "Hello from User",
     });
+  } catch (error) {}
+};
+
+module.exports.create = (req, res) => {
+  const { body } = req;
+  try {
   } catch (error) {}
 };
